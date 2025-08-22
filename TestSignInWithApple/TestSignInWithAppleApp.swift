56975910a4cf1d84d670e883587a76335b92dcb3
Garongan <alvindo56@gmail.com>
2025-08-22 14:03:55 +0700

    Initial Commit

---
+++ TestSignInWithApple/TestSignInWithAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSignInWithAppleApp.swift
+//  TestSignInWithApple
+//
+//  Created by Alvindo Tri Jatmiko on 22/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TestSignInWithAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
